fbc2a562e2cf8259c474e5ec74f6c9a86bfa6123
Zoran Lazarevic <lazarevic.zoki91@gmail.com>
2019-08-03 10:20:14 +0200

    moved functions to utils

--- main.go
+++ main.go
@@ -17,6 +17,36 @@ const width int = 300
 
 var activeGames = make(map[string]*Game)
 
+func main() {
+	PORT := os.Getenv("PORT")
+	if PORT == "" {
+		PORT = "8080"
+	}
+	rand.Seed(time.Now().UnixNano())
+
+	flag.Parse()
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", serveHome)
+	router.HandleFunc("/new", createGame)
+	router.HandleFunc("/g/{gameID}", serveGame)
+	router.HandleFunc("/ws/{gameID}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		key := vars["gameID"]
+
+		game := activeGames[key]
+		if game != nil {
+			connect(game, w, r)
+		}
+	})
+	http.Handle("/src/", http.StripPrefix("/src/", http.FileServer(http.Dir("./scripts"))))
+	http.Handle("/", router)
+	err := http.ListenAndServe(":"+PORT, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
 func serveHome(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.Error(w, "Not found", http.StatusNotFound)
@@ -53,44 +83,4 @@ func serveGame(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	http.ServeFile(w, r, "game.html")
-}
-
-func main() {
-	PORT := os.Getenv("PORT")
-	if PORT == "" {
-		PORT = "8000"
-	}
-	rand.Seed(time.Now().UnixNano())
-
-	flag.Parse()
-
-	router := mux.NewRouter()
-	router.HandleFunc("/", serveHome)
-	router.HandleFunc("/new", createGame)
-	router.HandleFunc("/g/{gameID}", serveGame)
-	router.HandleFunc("/ws/{gameID}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		key := vars["gameID"]
-
-		game := activeGames[key]
-		if game != nil {
-			connect(game, w, r)
-		}
-	})
-	http.Handle("/src/", http.StripPrefix("/src/", http.FileServer(http.Dir("./scripts"))))
-	http.Handle("/", router)
-	err := http.ListenAndServe(":"+PORT, nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
-}
-
-func getStartRotation() int {
-	return rand.Intn(360)
-}
-
-func randToken() string {
-	b := make([]byte, 8)
-	rand.Read(b)
-	return fmt.Sprintf("%x", b)
 }
